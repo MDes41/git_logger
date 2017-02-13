39bc73667e20128ec32befba7e57a236ec0299c6
Matt D <MDes41@users.noreply.github.com>
2017-02-13 11:00:57 -0700

    Looking through logs

--- app/controllers/api/v1/commits_controller.rb
+++ app/controllers/api/v1/commits_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::CommitsController < ApplicationController
 
 
 	def create
-		puts body_params
+		puts 'HHHLHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH' + body_params
 		render json: "Hello World", status: 200
 	end
 
